80543bc37e5912db635f37770708f2898538809c
Chetan Gupta <cg439@cornell.edu>
2015-05-14 21:28:05 +0000

    waf scripts for cgicc

--- waf-extras/cgicc.py
+++ waf-extras/cgicc.py
@@ -25,19 +25,19 @@ When using this tool, the wscript will look like:
 USE_NAME = 'CGICC'
 
 # List of shared library names to check for
-LIBS = []
+LIBS = ['libcgicc.so']
 
 # List of static library names to check for
-STLIBS = LIBS
+STLIBS = ['libcgicc.a']
 
 # List of header files to check for
-HEADER_NAMES = ['']
+HEADER_NAMES = ['cgicc/Cgicc.h', 'cgicc/CgiDefs.h']
 
 # List of paths to search for shared libraries, in order of preference
-LIBPATHS = []
+LIBPATHS = ['/lib', '/usr/lib', '/usr/local/lib']
 
 # List of paths to search for static libraries, in order of preference
-STLIBPATHS = LIBS
+STLIBPATHS = LIBPATHS
 
 # List of include paths
 INCLUDES = ['/include', '/usr/include', '/usr/local/include']
@@ -68,9 +68,9 @@ from waflib.Logs      import pprint
 from waflib.Utils     import unversioned_sys_platform
 
 @conf
-def download_eigen(ctx):
+def download_cgicc(ctx):
     # First, check if the source has already been downloaded
-    download_path = ctx.env.DEPS_DOWNLOADS + '/3.2.4.tar.gz'
+    download_path = ctx.env.DEPS_DOWNLOADS + '/cgicc-3.0.tar.gz'
     ctx.start_msg('Searching for ' + USE_NAME + ' download')
     download_node = ctx.root.find_node(download_path)
     if not download_node:
@@ -80,7 +80,7 @@ def download_eigen(ctx):
         ctx.msg('Downloading ' + USE_NAME, '...', color='YELLOW')
         command = ctx.env.CURL + ['--progress-bar', '--create-dirs', '-Lf',
                                   '-o', download_path,
-                'http://bitbucket.org/eigen/eigen/get/3.2.4.tar.gz']
+                'ftp://ftp.gnu.org/gnu/cgicc/cgicc-3.0.tar.gz']
         call(command, bufsize=0)
     else:
         ctx.end_msg('ok')
@@ -90,7 +90,7 @@ def download_eigen(ctx):
     source_dir_node.mkdir()
     # Then untar the download into it
     ctx.find_dep_program('tar')
-    command = ctx.env.TAR + ['-xf', download_path, '-C', ctx.env.DEPS_SRC]
+    command = ctx.env.TAR + ['-xvf', download_path, '-C', ctx.env.DEPS_SRC]
     msg = 'Extracting ' + USE_NAME + ' sources'
     ctx.exec_with_msg(command, msg)
 
@@ -100,13 +100,13 @@ def install_cgicc(ctx):
     pprint('YELLOW', 'Attempting ' + USE_NAME + ' auto-installation ...')
 
     # First, search for the source files
-    source_path = ctx.env.DEPS_SRC + '/eigen-eigen-10219c95fe65'
+    source_path = ctx.env.DEPS_SRC #+ '/eigen-eigen-10219c95fe65'
     ctx.start_msg('Searching for ' + USE_NAME + ' sources')
     source_node = ctx.root.find_node(source_path)
     # If sources not found, try to download them
     if not source_node:
         ctx.end_msg('not found', color='YELLOW')
-        ctx.download_eigen()
+        ctx.download_cgicc()
     else:
         ctx.end_msg('ok')
 
@@ -129,6 +129,6 @@ def configure(ctx):
         stlibs             = STLIBS,
         use_name           = USE_NAME,
         use_libs           = None,
-        installed_includes = ctx.env.DEPS_SRC + '/eigen-eigen-10219c95fe65',
+        installed_includes = ctx.env.DEPS_SRC,# + '/cgicc-cgicc-10219c95fe65',
         installed_libpaths = None
     )
